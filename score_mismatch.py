e3caa6f975c49a898ea6708e042142766931b24d
Manavalan Gajapathy <mgajapathy@hudsonalpha.org>
2017-06-30 21:50:19 -0500

    Prepares for HTML output - Part 1

--- score_mismatch.py
+++ score_mismatch.py
@@ -5,6 +5,7 @@ import numpy as np
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 100000)
+pd.set_option('max_colwidth', 100000)
 
 def example_df():
     # aa = pd.DataFrame({'a': [1,2], 'b':[3,4], 'c':[5,2]})
@@ -52,12 +53,73 @@ def booleante(alignment_pd, query_pos_aligned, ref_id):
 
     return query_pd, bool_query_pd, inverse_bool_query_pd
 
+
+def write_html(alignment_pd, id_maxLength):
+    alignment_len = len(alignment_pd.columns)
+    block_size = 60
+    if alignment_len % block_size:
+        no_of_blocks = (alignment_len/block_size + 1)
+    else:
+        no_of_blocks = alignment_len/block_size
+
+    with open('aa.txt', 'w') as handle:
+
+        html_blocks_pd = pd.DataFrame(index=alignment_pd.index)
+        for block_no in range(0, no_of_blocks):
+            min = block_no * block_size
+            max = (block_no +1) * block_size
+            if alignment_len <= max:
+                max = alignment_len
+            # print range(min, max)
+            # print
+
+            html_blocks_pd[block_no] = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
+            # html_block = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
+
+        # print html_blocks_pd
+
+                # print type(html_blocks_pd[[0]])
+                # print html_blocks_pd.index.str.len().max()
+            for i, item in html_blocks_pd[block_no].iteritems():
+                handle.write('%s    <spaced>%s</spaced>\n' % (i.ljust(id_maxLength), item))
+
+            handle.write('\n\n')
+                # print i, '\t\t2222\t', row
+            # xx = html_blocks_pd[[0]].to_string(index=False, justify='right', header=None)
+            # handle.write(xx)
+
+
+def process_for_html(bool_query_pd, alignment_pd, query_pos_aligned):
+    # alignment_len = len(alignment_pd)
+
+    print 'adsf'
+    bool_query_pd = bool_query_pd.T
+    alignment_pd = alignment_pd.T
+    id_maxLength = alignment_pd.index.str.len().max()
+
+    match_prefix = '<strong><span style="background-color: green">'
+    match_suffix = '</span></strong>'
+    mismatch_prefix = '<strong><span style="background-color: red">'
+
+    for pos in query_pos_aligned:
+        prefix = bool_query_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
+        suffix = bool_query_pd[pos].apply(lambda x:match_suffix)
+
+        alignment_pd[pos] = prefix + alignment_pd[pos] + suffix
+
+    write_html(alignment_pd, id_maxLength)
+
+
+
+
 # def xxxx():
 if __name__ == '__main__':
     ref_id = 'id_1|a'
-    query_pos_aligned = [0,1,2]
+    query_pos_aligned = [1,2]
     aligned_data = AlignIO.read('test_data/aligned.fasta', "fasta")
     alignment_pd = pc.from_bioalignment(aligned_data)       # converted to pandas dataframe
+
+
 
     # changes datatype of all columns from category to object
     for col_name in alignment_pd.columns:
@@ -68,42 +130,60 @@ if __name__ == '__main__':
     seq_length = alignment_pd[alignment_pd != '-'].count()
     results_csv_pd = csv_data(query_pd, bool_query_pd, inverse_bool_query_pd, seq_length)
 
+    process_for_html(bool_query_pd, alignment_pd, query_pos_aligned)
 
 
-def xxwef():
-    seq_length = alignment_pd[alignment_pd!='-'].count()
 
+    # if True:    # for html output
+    if False:
+        bool_query_pd = bool_query_pd.T
+        alignment_pd = alignment_pd.T
+        id_maxLength = alignment_pd.index.str.len().max()
 
-    query_df = alignment_pd.loc[query_pos_aligned, :]
-    print query_df
+        match_prefix = '<strong><span style="background-color: green">'
+        match_suffix = '</span></strong>'
+        mismatch_prefix = '<spaced><strong><span style="background-color: red">'
 
+        for pos in query_pos_aligned:
+            prefix = bool_query_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
+            suffix = bool_query_pd[pos].apply(lambda x:match_suffix)
 
-    bool_query_df = query_df.apply(lambda x:x==query_df[ref_id])
-    bool_query_inverse_df = query_df.apply(lambda x:x!=query_df[ref_id])
-    print bool_query_df
-    # bool_query_df = bool_query_df.astype(int)
+            alignment_pd[pos] = prefix + alignment_pd[pos] + suffix
 
-    mismatch_count = bool_query_df[bool_query_df<1].count()
+        # print alignment_pd[[0,1,2,3]]
 
-    # print query_df.values * bool_query_df.values
-    # print pd.DataFrame(query_df.values*bool_query_df, columns=query_df.columns, index=query_df.index)
-    mult = query_df * bool_query_inverse_df
+        # alignment_pd['merged'] = alignment_pd[range(5)].apply(lambda x: ''.join(x), axis=1)
+        # print alignment_pd['merged']
 
-    # for col_name in alignment_pd.columns:
-    #     mult.ix[mult[col_name] == '', col_name] = '='
-    # out_pd = mult.T
-    # out_pd['mismatch_count'] = mismatch_count
-    # out_pd['seq_length'] = seq_length
+        # alignment_len = 61
+        block_size = 30
+        if alignment_len % block_size:
+            no_of_blocks = (alignment_len/block_size + 1)
+        else:
+            no_of_blocks = alignment_len/block_size
 
-    # out_pd['title'] = out_pd.index
-    title_df = out_pd['title'].str.split('|', expand=True)
-    # print pd.Series(title_df.columns, dtype=str)
+        with open('aa.txt', 'w') as handle:
 
-    title_df.columns = pd.Series(['a']*2).map(str) + '-' + pd.Series(title_df.columns).map(str)
-    print title_df.columns
-    # out_pd.drop(['title'])
-    out_pd.drop('title', axis=1, inplace=True)
+            html_blocks_pd = pd.DataFrame(index=alignment_pd.index)
+            for block_no in range(0, no_of_blocks):
+                min = block_no * block_size
+                max = (block_no +1) * block_size
+                if alignment_len <= max:
+                    max = alignment_len
+                # print range(min, max)
+                # print
 
-    final = pd.concat([title_df, out_pd], axis=1)
-    # print final.columns
+                html_blocks_pd[block_no] = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
+                # html_block = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
 
+            # print html_blocks_pd
+
+                    # print type(html_blocks_pd[[0]])
+                    # print html_blocks_pd.index.str.len().max()
+                for i, item in html_blocks_pd[block_no].iteritems():
+                    handle.write('%s    %s\n' % (i.ljust(id_maxLength), item))
+
+                handle.write('\n\n')
+                    # print i, '\t\t2222\t', row
+                # xx = html_blocks_pd[[0]].to_string(index=False, justify='right', header=None)
+                # handle.write(xx)
