c3a5f162787398aabbf8fe6b28e6c663d7acd359
Manavalan Gajapathy <mgajapathy@hudsonalpha.org>
2017-07-02 16:46:17 -0500

    Rearranges functions and clean-up a bit

--- score_mismatch.py
+++ score_mismatch.py
@@ -8,6 +8,8 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 100000)
 pd.set_option('max_colwidth', 100000)
 
+pd.options.mode.chained_assignment = None  # default='warn'
+
 def example_df():
     # aa = pd.DataFrame({'a': [1,2], 'b':[3,4], 'c':[5,2]})
     # aa = pd.DataFrame({'a': ['1','2', '7'], 'b':['3','4', '8'], 'c':['2','2', '8']})
@@ -22,102 +24,21 @@ def example_df():
         print x
 
 
-def split_id_by_delimiter(results_csv_pd):
-    title_df = pd.DataFrame(index=results_csv_pd.index)
-    title_df['title'] = results_csv_pd.index
-    title_df = title_df['title'].str.split('|', expand=True)  # split using a delimiter symbol
-    title_df.columns = pd.Series(['Title'] * 2).map(str) + '_' + pd.Series(title_df.columns).map(str)
-
-    return title_df
-
-
-def csv_data(query_pd, bool_query_pd, inverse_bool_query_pd, seq_length):
-    results_csv_pd = query_pd.T * inverse_bool_query_pd.T
-    for col_name in results_csv_pd.columns:
-        results_csv_pd.ix[(results_csv_pd[col_name]==''), col_name] = '='
-
-    results_csv_pd['mismatch_count'] = bool_query_pd[bool_query_pd==0].count()
-    results_csv_pd['seq_length'] = seq_length
-
-    title_df = split_id_by_delimiter(results_csv_pd)
-
-    results_csv_pd = pd.concat([title_df, results_csv_pd], axis=1)
-
-    return results_csv_pd
-
-
-def booleante(alignment_pd, query_pos_aligned, ref_id):
-    query_pd = alignment_pd.loc[query_pos_aligned, :]
-
-    bool_query_pd = query_pd.apply(lambda x:x==query_pd[ref_id])
-    inverse_bool_query_pd = query_pd.apply(lambda x:x!=query_pd[ref_id])    # True and False are switched
-
-    return query_pd, bool_query_pd, inverse_bool_query_pd
-
-
-def query_pos_index(series):
-    print series
-    pass
-
-def write_html(alignment_pd, id_maxLength):
-    alignment_len = len(alignment_pd.columns)
-    block_size = 60
-    if alignment_len % block_size:
-        no_of_blocks = (alignment_len/block_size + 1)
-    else:
-        no_of_blocks = alignment_len/block_size
-
-    with open('aa.txt', 'w') as handle:
-
-        html_blocks_pd = pd.DataFrame(index=alignment_pd.index)
-        for block_no in range(0, no_of_blocks):
-            min = block_no * block_size
-            max = (block_no +1) * block_size
-            if alignment_len <= max:
-                max = alignment_len
-
-            html_blocks_pd[block_no] = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
-
-            for i, item in html_blocks_pd[block_no].iteritems():
-                handle.write('%s    <spaced>%s</spaced>\n' % (i.ljust(id_maxLength), item))
-
-            handle.write('\n\n')
-
-    # query_pos_index(html_blocks_pd[block_no])
-
-
-def process_for_html(bool_query_pd, alignment_pd, query_pos_aligned):
-    # alignment_len = len(alignment_pd)
-
-    bool_query_pd = bool_query_pd.T
-    alignment_pd = alignment_pd.T
-    id_maxLength = alignment_pd.index.str.len().max()
-
-    match_prefix = '<strong><span style="background-color: #42DB33">'
-    match_suffix = '</span></strong>'
-    mismatch_prefix = '<strong><span style="background-color: #F73D94">'
-
-    for pos in query_pos_aligned:
-        prefix = bool_query_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
-        suffix = bool_query_pd[pos].apply(lambda x:match_suffix)
-
-        alignment_pd[pos] = prefix + alignment_pd[pos] + suffix
-
-    write_html(alignment_pd, id_maxLength)
-
-
+# converts fasta files to pandas dataframes
 def fasta_to_pandas(f):
     data_fasta = AlignIO.read(f, "fasta")
     data_pd = pc.from_bioalignment(data_fasta)       # converted to pandas dataframe
-    # data_pd = data_pd.astype(object)
+
+    # convert categories datatype to objects
     for col_name in data_pd.columns:
         data_pd[col_name] = data_pd[col_name].astype(object)
-
 
     return data_pd
 
 
-
+# Makes following checks with respect to reference sequence:
+# 1. Does reference file has only one sequence? Is its ID part of Sequences file?
+# 2. Does seq in Reference file and corresponding seq in Sequence file match?
 def check_ref_seq_in_alignment(reference_pd, alignment_pd):
     ref_id = list(reference_pd.columns)
     alignment_IDs = list(alignment_pd.columns)
@@ -151,60 +72,165 @@ def check_ref_seq_in_alignment(reference_pd, alignment_pd):
     return ref_id, ref_seq_in_alignment
 
 
+# Based on query positions in input, make pandas dataframes - reading reference file and alignment file
 def get_query_residue_info(query_pos_actual, reference_pd, ref_id, ref_seq_in_alignment):
     query_pos_0indexed = [x-1  for x in query_pos_actual]
 
     # get query residues
-    query_residues_pd = reference_pd.iloc[query_pos_0indexed, :]
-    query_residues_pd['actual_pos'] = query_residues_pd.index + 1
+    query_ref_pd = reference_pd.iloc[query_pos_0indexed, :]
+    query_ref_pd['actual_pos'] = query_ref_pd.index + 1
 
-    query_info_in_alignment_pd = ref_seq_in_alignment.iloc[query_residues_pd.index, :]
+    query_info_in_alignment_pd = ref_seq_in_alignment.iloc[query_ref_pd.index, :]
     query_info_in_alignment_pd.rename(columns={ref_id : 'query_residues', 'index_in_alignment': 'pos_in_alignment'}, inplace=True)
     query_info_in_alignment_pd['query_pos_actual'] = query_info_in_alignment_pd.index + 1
     query_info_in_alignment_pd['query_label_csv'] = query_info_in_alignment_pd['query_pos_actual'].astype(str) + ' "' + query_info_in_alignment_pd['query_residues'] + '"'
     query_info_in_alignment_pd['query_label_html'] = query_info_in_alignment_pd['query_pos_actual'].astype(str) + '<BR>' + query_info_in_alignment_pd['query_residues']
-    print query_info_in_alignment_pd
+    query_info_in_alignment_pd['query_line_html'] = '<a name="' + query_info_in_alignment_pd['query_pos_actual'].astype(str) \
+                                                    + '"><vert>' + query_info_in_alignment_pd['query_pos_actual'].astype(str) + '</vert></a>'
 
-    return query_residues_pd, query_info_in_alignment_pd
+    return query_ref_pd, query_info_in_alignment_pd
+
+
+# Get DF of alignment at query positions. Match query seqs to reference seq, and return a boolean DF.
+def booleante(alignment_pd, query_pos_aligned, ref_id):
+    query_region_pd = alignment_pd.loc[query_pos_aligned, :]
+
+    bool_query_region_pd = query_region_pd.apply(lambda x:x==query_region_pd[ref_id])
+    inverse_bool_query_region_pd = query_region_pd.apply(lambda x:x!=query_region_pd[ref_id])    # True and False are switched
+
+    return query_region_pd, bool_query_region_pd, inverse_bool_query_region_pd
+
+
+def split_id_by_delimiter(results_csv_pd):
+    title_df = pd.DataFrame(index=results_csv_pd.index)
+    title_df['title'] = results_csv_pd.index
+    title_df = title_df['title'].str.split('|', expand=True)  # split using a delimiter symbol
+    title_df.columns = pd.Series(['Title'] * 2).map(str) + '_' + pd.Series(title_df.columns).map(str)
+
+    return title_df
+
+
+def write_csv_out(results_csv_pd):
+    print results_csv_pd
+
+    match_only_pd = results_csv_pd[results_csv_pd['mismatch_count']==0]
+    # print match_only_pd
+
+    mismatch_only_pd = results_csv_pd[results_csv_pd['mismatch_count']!=0]
+    # mismatch_only_pd['S.No'] = [x for x in range(1, len(mismatch_only_pd)+1)]
+    mismatch_only_pd.insert(0, 'S.No', [x for x in range(1, len(mismatch_only_pd)+1)])
+    print mismatch_only_pd
+
+def prepare_for_csv_output(query_region_pd, bool_query_region_pd, inverse_bool_query_region_pd, seq_length_alignment_pd):
+    results_csv_pd = query_region_pd.T * inverse_bool_query_region_pd.T
+
+    for col_name in results_csv_pd.columns:
+        results_csv_pd.ix[(results_csv_pd[col_name]==''), col_name] = '='
+
+    results_csv_pd['mismatch_count'] = bool_query_region_pd[bool_query_region_pd==0].count()
+    results_csv_pd['seq_length_alignment_pd'] = seq_length_alignment_pd
+
+    title_df = split_id_by_delimiter(results_csv_pd)
+
+    results_csv_pd = pd.concat([title_df, results_csv_pd], axis=1)
+
+    write_csv_out(results_csv_pd)
+
+    return results_csv_pd
+
+
+def write_html(alignment_pd, id_maxLength, query_info_in_alignment_pd):
+    alignment_len = len(alignment_pd.columns)
+    block_size = 60
+    if alignment_len % block_size:
+        no_of_blocks = (alignment_len/block_size + 1)
+    else:
+        no_of_blocks = alignment_len/block_size
+
+    with open('aa.txt', 'w') as handle:
+
+        query_line_pd = pd.DataFrame(index=alignment_pd.columns)
+        query_line_pd['trial'] = ' '
+        # print query_info_in_alignment_pd
+        query_line_pd.loc[(query_info_in_alignment_pd['pos_in_alignment']), 'trial'] = list(query_info_in_alignment_pd['query_line_html'])
+        query_line_pd['trial'].fillna(' ')
+
+
+        alignment_pd = pd.concat([query_line_pd.T, alignment_pd], axis=0)
+        html_blocks_pd = pd.DataFrame(index=alignment_pd.index)
+        for block_no in range(0, no_of_blocks):
+            min = block_no * block_size
+            max = (block_no +1) * block_size
+            if alignment_len <= max:
+                max = alignment_len
+
+            html_blocks_pd[block_no] = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
+            # print html_blocks_pd
+
+            for i, item in html_blocks_pd[block_no].iteritems():
+                # print i
+                handle.write('%s    <spaced>%s</spaced>\n' % (i.ljust(id_maxLength), item))
+
+            handle.write('\n\n')
+
+    # query_pos_index(html_blocks_pd[block_no])
+
+
+def process_for_html(bool_query_region_pd, alignment_pd, query_pos_aligned, query_info_in_alignment_pd):
+    # alignment_len = len(alignment_pd)
+
+    bool_query_region_pd = bool_query_region_pd.T
+    alignment_pd = alignment_pd.T
+    id_maxLength = alignment_pd.index.str.len().max()
+
+    match_prefix = '<strong><span style="background-color: #42DB33">'
+    match_suffix = '</span></strong>'
+    mismatch_prefix = '<strong><span style="background-color: #F73D94">'
+
+    for pos in query_pos_aligned:
+        prefix = bool_query_region_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
+        suffix = bool_query_region_pd[pos].apply(lambda x:match_suffix)
+
+        alignment_pd[pos] = prefix + alignment_pd[pos] + suffix
+
+    write_html(alignment_pd, id_maxLength, query_info_in_alignment_pd)
 
 
 
 # def xxxx():
 if __name__ == '__main__':
-    query_pos_actual = [1,3]
+    query_pos_actual = [1,2,3,4,5]
 
     reference_f = 'test_data/Reference.fasta'
     alignment_f = 'test_data/aligned.fasta'
+
     reference_pd = fasta_to_pandas(reference_f)
     alignment_pd = fasta_to_pandas(alignment_f)
 
     # check reference sequence criteria pass prelimiary test
     ref_id, ref_seq_in_alignment = check_ref_seq_in_alignment(reference_pd, alignment_pd)
 
-    query_residues_pd, query_info_in_alignment_pd = get_query_residue_info(query_pos_actual, reference_pd, ref_id, ref_seq_in_alignment)
+    # process query pos and residue info in reference seq and in alignment
+    query_ref_pd, query_info_in_alignment_pd = get_query_residue_info(query_pos_actual, reference_pd, ref_id, ref_seq_in_alignment)
 
-    # query_pos_aligned = query_info_in_alignment_pd['index'].tolist()
+    # get query positions in alignment
+    query_pos_aligned = list(query_info_in_alignment_pd['pos_in_alignment'])
 
-    query_pos_aligned = [1,2]
+    # get query region of alignment
+    query_region_pd, bool_query_region_pd, inverse_bool_query_region_pd = booleante(alignment_pd, query_pos_aligned, ref_id)
 
+    # get length of sequences in alignment
+    seq_length_alignment_pd = alignment_pd[alignment_pd != '-'].count()
 
+    results_csv_pd = prepare_for_csv_output(query_region_pd, bool_query_region_pd, inverse_bool_query_region_pd, seq_length_alignment_pd)
 
-    # changes datatype of all columns from category to object
-    for col_name in alignment_pd.columns:
-        alignment_pd[col_name] = alignment_pd[col_name].astype(object)
-
-    query_pd, bool_query_pd, inverse_bool_query_pd = booleante(alignment_pd, query_pos_aligned, ref_id)
-
-    seq_length = alignment_pd[alignment_pd != '-'].count()
-    results_csv_pd = csv_data(query_pd, bool_query_pd, inverse_bool_query_pd, seq_length)
-
-    process_for_html(bool_query_pd, alignment_pd, query_pos_aligned)
+    process_for_html(bool_query_region_pd, alignment_pd, query_pos_aligned, query_info_in_alignment_pd)
 
 
 
     # if True:    # for html output
     if False:
-        bool_query_pd = bool_query_pd.T
+        bool_query_region_pd = bool_query_region_pd.T
         alignment_pd = alignment_pd.T
         id_maxLength = alignment_pd.index.str.len().max()
 
@@ -213,8 +239,8 @@ if __name__ == '__main__':
         mismatch_prefix = '<spaced><strong><span style="background-color: #F73D94">'
 
         for pos in query_pos_aligned:
-            prefix = bool_query_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
-            suffix = bool_query_pd[pos].apply(lambda x:match_suffix)
+            prefix = bool_query_region_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
+            suffix = bool_query_region_pd[pos].apply(lambda x:match_suffix)
 
             alignment_pd[pos] = prefix + alignment_pd[pos] + suffix
 
