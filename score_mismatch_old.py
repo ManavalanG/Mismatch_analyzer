6c090dc79faf92a3b4f13d04f7531a940de85b75
Manavalan Gajapathy <mgajapathy@hudsonalpha.org>
2017-07-02 22:41:07 -0500

    Adds option to run clustal omega MSA

--- score_mismatch_old.py
+++ score_mismatch_old.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pandascharm as pc
 import numpy as np
 from shutil import copyfile
-from run_clustalo import run_clustal_omega
+
 
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 100000)
@@ -198,85 +198,7 @@ def write_csv_out(results_csv_pd, unique_pd):
         match_only_pd.to_csv(csv_handle, sep='\t', index=False)
 
 
-def html_chart_text(info_lines, width_canvas, query_site_actual, identity_perc_list, out_html_handle):
-    text_for_chart = ('</HEAD>\n<BODY LANG="en-US" DIR="LTR">\n'
-                      '<PRE CLASS="western">\n%s'
-                      '<div>\n  <canvas id="canvas_bar" height="350", width = "%i" ></canvas>\n'
-                      '<div id="legend"></div>\n</div>\n\n<script>\n'
-                      '\tvar barChartData = \n\t{\n'
-                      '\t\tlabels : %s,\n'
-                      '\t\tdatasets : [\n'
-                      '\t\t{	fillColor : "rgba(51,51,255,0.8)",\n\t\t\ttitle: "%% Identity",\n'
-                      '\t\t\tdata : %s  },\n\n'
-                      % (info_lines, width_canvas, query_site_actual, identity_perc_list))
-
-    out_html_handle.write(text_for_chart)
-    out_html_handle.write('\t\t]\n\t}\n')
-
-
-def html_table_text(query_site_residues, query_site_actual, identity_perc_list, out_html_handle):
-    limit_col = 20  # this variable determines how many columns are allowed in each table
-    no_of_tables = (len(query_site_residues) - 1 + limit_col) / limit_col
-    limit_col_temp = limit_col
-
-    row1 = []
-    for n in range(0, len(query_site_actual)):
-        temp = '<a href="#%i">%i<BR>%s</a>' % (
-            query_site_actual[n], query_site_actual[n], query_site_residues[n])  # hyperlink included
-        row1.append(temp)
-    row2 = []
-    for n in range(0, len(identity_perc_list)):
-        row2.append("%3.1f" % identity_perc_list[n])
-
-    rows = [row1, row2]
-    heading = ['Position', '% Identity', '% Conservation']
-    for table_no in range(0, no_of_tables):
-        table_string = '<TABLE border="1" cellpadding="0" cellspacing="0" HEIGHT="90px" style="table-layout:fixed">\n'
-        if table_no < (no_of_tables - 1):
-            end_col = limit_col_temp
-        else:
-            end_col = len(query_site_residues)
-
-        for no in range(0, len(rows)):
-            if not no:
-                table_string += ('\t<TR ALIGN="CENTER" BGCOLOR="#E3E3E0">\n')
-            else:
-                table_string += ('\t<TR ALIGN="CENTER">\n')
-
-            table_string += ('\t\t<TH scope="row" width=130>%s</TH>\n' % heading[no])
-            for ele in range((limit_col_temp - limit_col), end_col):
-                table_string += ('\t\t<TD width=60>%s</TD>\n' % rows[no][ele])
-
-            table_string += '\t</TR>\n'
-        table_string += ('</TABLE>\n')
-        out_html_handle.write(table_string)
-        limit_col_temp += limit_col
-
-
-
-def html_color_legend(match_color, similar_color, mismatch_color, out_html_handle):
-    temp = '-' * 100 + '\n\n'
-    temp += '<strong> Color           For Residues                      For Sequence IDs  </strong>\n\n'
-    temp += (
-    '<span style="background-color: %s">        </span>      Matching                        Seq. has no mismatches at all\n\n' % match_color)
-
-    protein_mode = False
-    if protein_mode:
-        temp += (
-        '<span style="background-color: %s">        </span>      Mismatching but <ins>similar</ins>         Seq. has at least one mismatch but <ins>similar</ins> residue\n\n' % similar_color)
-        temp += (
-        '<span style="background-color: %s">        </span>      Mismatching and <ins>dissimilar</ins>      Seq. has at least one mismatch and <ins>dissimilar</ins> residue\n\n' % mismatch_color)
-
-    else:
-        temp += (
-        '<span style="background-color: %s">        </span>      Mismatching                     Seq. has at least one mismatching residue\n\n' % mismatch_color)
-
-    temp += "<span style='background-color: #00FFFF'>        </span>      Not applicable                  Reference Sequence's ID\n\n"
-
-    out_html_handle.write(temp + '-' * 100 + '\n\n')
-
-
-def html_alignment_text(alignment_pd, id_maxLength, query_info_in_alignment_pd, out_html_handle):
+def write_html(alignment_pd, id_maxLength, query_info_in_alignment_pd):
     alignment_len = len(alignment_pd.columns)
     block_size = 60
     if alignment_len % block_size:
@@ -284,8 +206,7 @@ def html_alignment_text(alignment_pd, id_maxLength, query_info_in_alignment_pd, 
     else:
         no_of_blocks = alignment_len/block_size
 
-    # with open('aa.txt', 'w') as handle:
-    if True:
+    with open('aa.txt', 'w') as handle:
 
         query_line_pd = pd.DataFrame(index=alignment_pd.columns)
         query_line_pd['trial'] = ' '
@@ -294,93 +215,44 @@ def html_alignment_text(alignment_pd, id_maxLength, query_info_in_alignment_pd, 
         query_line_pd['trial'].fillna(' ')
 
 
-        # alignment_pd = pd.concat([query_line_pd.T, alignment_pd], axis=0)
+        alignment_pd = pd.concat([query_line_pd.T, alignment_pd], axis=0)
         html_blocks_pd = pd.DataFrame(index=alignment_pd.index)
-        print html_blocks_pd
         for block_no in range(0, no_of_blocks):
             min = block_no * block_size
             max = (block_no +1) * block_size
             if alignment_len <= max:
                 max = alignment_len
 
-            print alignment_pd[range(min, max)], '\n'
             html_blocks_pd[block_no] = alignment_pd[range(min, max)].apply(lambda x: ''.join(x), axis=1)
             # print html_blocks_pd
 
             for i, item in html_blocks_pd[block_no].iteritems():
                 # print i
-                out_html_handle.write('%s    <spaced>%s</spaced>\n' % (i.ljust(id_maxLength), item))
+                handle.write('%s    <spaced>%s</spaced>\n' % (i.ljust(id_maxLength), item))
 
-            out_html_handle.write('\n\n')
+            handle.write('\n\n')
 
     # query_pos_index(html_blocks_pd[block_no])
 
 
-def html_text_out(alignment_pd, id_maxLength, query_info_in_alignment_pd, unique_pd, match_color, similar_color, mismatch_color):
-    print query_info_in_alignment_pd
-    print unique_pd
-    # print list(query_info_in_alignment_pd['query_pos_in_ref'])
-
-    html_template_head_f = '/Users/mana/Documents/GitHub_downloaded/Mismatch_analyzer/test_data/Template_html5_ChartNewjs_head.txt'
-    html_template_tail_f = '/Users/mana/Documents/GitHub_downloaded/Mismatch_analyzer/test_data/Template_html5_ChartNewjs_tail.txt'
-    html_outfile = 'html_out.html'
-
-    # write starting block of html file to outfile
-    copyfile(html_template_head_f, html_outfile)
-
-    with open(html_outfile, 'a') as out_html_handle, open(html_template_tail_f, 'r') as html_tail_handle:
-        out_html_handle.write('sadfghj\sdfgh\dsfghj\n')
-
-        info_lines ='***info_lines***'
-        width_canvas = 325
-        # query_site_actual = [1,2,3]
-        # query_site_residues = ['A', 'D', 'R']
-        # identity_perc_list = [100, 33, 50]
-
-        query_site_actual = list(query_info_in_alignment_pd['query_pos_actual'])
-        query_site_residues = list(query_info_in_alignment_pd['query_residues'])
-        identity_perc_list = list(unique_pd['% Identity'])
-
-
-        # makes chart
-        html_chart_text(info_lines, width_canvas, query_site_actual, identity_perc_list, out_html_handle)
-
-        # write tailing text
-        out_html_handle.write(html_tail_handle.read())
-
-        #write table data
-        html_table_text(query_site_residues, query_site_actual, identity_perc_list, out_html_handle)
-
-        # write color legend data
-        html_color_legend(match_color, similar_color, mismatch_color, out_html_handle)
-
-        # write alignment text
-        html_alignment_text(alignment_pd, id_maxLength, query_info_in_alignment_pd, out_html_handle)
-
-
-
-def process_for_html(bool_query_region_pd, alignment_pd, query_pos_aligned, query_info_in_alignment_pd, unique_pd):
+def process_for_html(bool_query_region_pd, alignment_pd, query_pos_aligned, query_info_in_alignment_pd):
     # alignment_len = len(alignment_pd)
 
     bool_query_region_pd = bool_query_region_pd.T
     alignment_pd = alignment_pd.T
     id_maxLength = alignment_pd.index.str.len().max()
 
-    match_color = '#42DB33'
-    similar_color = '#FFFF00'
-    mismatch_color = '#F73D94'
-
-    match_prefix = '<strong><span style="background-color: %s">' % match_color
-    suffix = '</span></strong>'
-    mismatch_prefix = '<strong><span style="background-color: %s">' % mismatch_color
+    match_prefix = '<strong><span style="background-color: #42DB33">'
+    match_suffix = '</span></strong>'
+    mismatch_prefix = '<strong><span style="background-color: #F73D94">'
 
     for pos in query_pos_aligned:
         prefix = bool_query_region_pd[pos].apply(lambda x:match_prefix if x else mismatch_prefix)
-        suffix = bool_query_region_pd[pos].apply(lambda x:suffix)
+        suffix = bool_query_region_pd[pos].apply(lambda x:match_suffix)
 
         alignment_pd[pos] = prefix + alignment_pd[pos] + suffix
 
-    html_text_out(alignment_pd, id_maxLength, query_info_in_alignment_pd, unique_pd, match_color, html_text_out, mismatch_color)
+    write_html(alignment_pd, id_maxLength, query_info_in_alignment_pd)
 
 
 
@@ -390,11 +262,6 @@ if __name__ == '__main__':
 
     reference_f = 'test_data/Reference.fasta'
     alignment_f = 'test_data/aligned.fasta'
-
-    # runs clustal omega alignment
-    if False:
-        seq_f = ''
-        run_clustal_omega(seq_f, alignment_f)
 
     reference_pd = fasta_to_pandas(reference_f)
     alignment_pd = fasta_to_pandas(alignment_f)
@@ -423,4 +290,4 @@ if __name__ == '__main__':
     # write csv output
     write_csv_out(results_csv_pd, unique_pd)
 
-    process_for_html(bool_query_region_pd, alignment_pd, query_pos_aligned, query_info_in_alignment_pd, unique_pd)
+    process_for_html(bool_query_region_pd, alignment_pd, query_pos_aligned, query_info_in_alignment_pd)
--- write_html_out.py
+++ write_html_out.py
@@ -1,32 +1,23 @@
 from shutil import copyfile
 
-html_template_head_f = '/Users/mana/Documents/GitHub_downloaded/Mismatch_analyzer/test_data/Template_html5_ChartNewjs_head.txt'
-html_template_tail_f = '/Users/mana/Documents/GitHub_downloaded/Mismatch_analyzer/test_data/Template_html5_ChartNewjs_tail.txt'
-html_outfile = 'html_out.html'
 
-copyfile(html_template_head_f, html_outfile)
+def html_chart_text(info_lines, width_canvas, query_site_actual, identity_perc_list, out_html_handle):
+    text_for_chart = ('</HEAD>\n<BODY LANG="en-US" DIR="LTR">\n'
+                      '<PRE CLASS="western">\n%s'
+                      '<div>\n  <canvas id="canvas_bar" height="350", width = "%i" ></canvas>\n'
+                      '<div id="legend"></div>\n</div>\n\n<script>\n'
+                      '\tvar barChartData = \n\t{\n'
+                      '\t\tlabels : %s,\n'
+                      '\t\tdatasets : [\n'
+                      '\t\t{	fillColor : "rgba(51,51,255,0.8)",\n\t\t\ttitle: "%% Identity",\n'
+                      '\t\t\tdata : %s  },\n\n'
+                      % (info_lines, width_canvas, query_site_actual, identity_perc_list))
 
-with open(html_outfile, 'a') as out_html_handle, open(html_template_tail_f, 'r') as html_tail_handle:
-    out_html_handle.write('sadfghj\sdfgh\dsfghj\n')
-
-    info_lines ='***info_lines***'
-    width_canvas = 325
-    query_site_actual = [1,2,3]
-    query_site_residues = ['A', 'D', 'R']
-    identity_perc_list = [100, 33, 50]
-    out_html_handle.write('</HEAD>\n<BODY LANG="en-US" DIR="LTR">\n<PRE CLASS="western">\n%s'
-                          '<div>\n  <canvas id="canvas_bar" height="350", width = "%i" ></canvas>\n'
-                          '<div id="legend"></div>\n</div>\n\n<script>\n'
-                          '\tvar barChartData = \n\t{\n'
-                          '\t\tlabels : %s,\n\t\tdatasets : [\n'
-                          '\t\t{	fillColor : "rgba(51,51,255,0.8)",\n\t\t\ttitle: "%% Identity",\n'
-                          '\t\t\tdata : %s  },\n\n'
-                          % (info_lines, width_canvas, query_site_actual, identity_perc_list))
+    out_html_handle.write(text_for_chart)
     out_html_handle.write('\t\t]\n\t}\n')
 
-    out_html_handle.write(html_tail_handle.read())
 
-
+def html_table_text(query_site_residues, query_site_actual, identity_perc_list, out_html_handle):
     limit_col = 20  # this variable determines how many columns are allowed in each table
     no_of_tables = (len(query_site_residues) - 1 + limit_col) / limit_col
     limit_col_temp = limit_col
@@ -34,7 +25,7 @@ with open(html_outfile, 'a') as out_html_handle, open(html_template_tail_f, 'r')
     row1 = []
     for n in range(0, len(query_site_actual)):
         temp = '<a href="#%i">%i<BR>%s</a>' % (
-        query_site_actual[n], query_site_actual[n], query_site_residues[n])  # hyperlink included
+            query_site_actual[n], query_site_actual[n], query_site_residues[n])  # hyperlink included
         row1.append(temp)
     row2 = []
     for n in range(0, len(identity_perc_list)):
@@ -65,3 +56,57 @@ with open(html_outfile, 'a') as out_html_handle, open(html_template_tail_f, 'r')
         limit_col_temp += limit_col
 
 
+
+def html_color_legend(match_color, similar_color, mismatch_color, out_html_handle):
+    temp = '-' * 100 + '\n\n'
+    temp += '<strong> Color           For Residues                      For Sequence IDs  </strong>\n\n'
+    temp += (
+    '<span style="background-color: %s">        </span>      Matching                        Seq. has no mismatches at all\n\n' % match_color)
+
+    protein_mode = False
+    if protein_mode:
+        temp += (
+        '<span style="background-color: %s">        </span>      Mismatching but <ins>similar</ins>         Seq. has at least one mismatch but <ins>similar</ins> residue\n\n' % similar_color)
+        temp += (
+        '<span style="background-color: %s">        </span>      Mismatching and <ins>dissimilar</ins>      Seq. has at least one mismatch and <ins>dissimilar</ins> residue\n\n' % mismatch_color)
+
+    else:
+        temp += (
+        '<span style="background-color: %s">        </span>      Mismatching                     Seq. has at least one mismatching residue\n\n' % mismatch_color)
+
+    temp += "<span style='background-color: #00FFFF'>        </span>      Not applicable                  Reference Sequence's ID\n\n"
+
+    out_html_handle.write(temp + '-' * 100 + '\n\n')
+
+
+# def asdf():
+if __name__ == '__main__':
+    html_template_head_f = '/Users/mana/Documents/GitHub_downloaded/Mismatch_analyzer/test_data/Template_html5_ChartNewjs_head.txt'
+    html_template_tail_f = '/Users/mana/Documents/GitHub_downloaded/Mismatch_analyzer/test_data/Template_html5_ChartNewjs_tail.txt'
+    html_outfile = 'html_out.html'
+
+    copyfile(html_template_head_f, html_outfile)
+
+    with open(html_outfile, 'a') as out_html_handle, open(html_template_tail_f, 'r') as html_tail_handle:
+        out_html_handle.write('sadfghj\sdfgh\dsfghj\n')
+
+        info_lines ='***info_lines***'
+        width_canvas = 325
+        query_site_actual = [1,2,3]
+        query_site_residues = ['A', 'D', 'R']
+        identity_perc_list = [100, 33, 50]
+
+        # makes chart
+        html_chart_text(info_lines, width_canvas, query_site_actual, identity_perc_list, out_html_handle)
+
+        # write tailing text
+        out_html_handle.write(html_tail_handle.read())
+
+        #write table data
+        html_table_text(query_site_residues, query_site_actual, identity_perc_list, out_html_handle)
+
+        # write color legend data
+        match_color = '#42DB33'
+        similar_color = '#FFFF00'
+        mismatch_color = '#F73D94'
+        html_color_legend(match_color, similar_color, mismatch_color, out_html_handle)
